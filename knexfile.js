72b9fcf9ea68aecb5cd8b05af5ef61769047eab5
Bruno <brunofellipe20@gmail.com>
2021-03-03 09:41:24 -0300

    criação de knex file e config do banco

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database:'KnexTeste',
+      user:'postgres',
+      password:'root'
+    }
+  },
+
+};
