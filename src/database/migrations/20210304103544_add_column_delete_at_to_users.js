8ae0dfd3862882757af2752fefa36f23e7376ab9
Bruno <brunofellipe20@gmail.com>
2021-03-04 10:42:01 -0300

    criacçãod e coluna para uso do soft-delete

--- src/database/migrations/20210304103544_add_column_delete_at_to_users.js
+++ src/database/migrations/20210304103544_add_column_delete_at_to_users.js
@@ -1,8 +1,8 @@
-
 exports.up = function(knex) {
-  
-};
+  return knex.schema.alterTable('users',table=>
+    table.timestamp('Deleted_at')    
+)};
 
-exports.down = function(knex) {
-  
-};
+exports.down = knex=>knex.schema.alterTable('users',table=>{
+  table.dropColumn('deleted_at')
+  })
