033dabf2bb2a1405e8a1d39ef75f73873328054a
Bruno <brunofellipe20@gmail.com>
2021-03-04 09:48:34 -0300

    insert update,delete error handling,controllers

--- src/server.js
+++ src/server.js
@@ -5,4 +5,17 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+//Not Found
+app.use((req,res,next)=>{
+  const error = new Error('not found')
+  error.status = 404
+  next(error)
+})
+
+//catch all
+app.use(( error,req,res,next )=>{
+  res.status( error.status || 500 )
+  res.json({ error: error.message })
+})
+
 app.listen(3333, ()=>console.log('Server is running! 👨‍🎤👨‍🎤'))
